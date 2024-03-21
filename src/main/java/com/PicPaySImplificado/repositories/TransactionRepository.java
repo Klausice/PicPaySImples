12b2ab7ffb8ee029efbb84a56c6a926b92e45ddd
klaus <klausic3@gmail.com>
2024-03-20 23:47:50 -0300

    feat: create TransactionRepository

---
+++ src/main/java/com/PicPaySImplificado/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.PicPaySImplificado.repositories;
+
+import com.PicPaySImplificado.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+}
