969e9634e243a6629f8c7b8cf23da2b907b66b05
klaus <klausic3@gmail.com>
2024-03-20 23:42:44 -0300

    feat: initial config

---
+++ src/main/java/com/PicPaySImplificado/PicPaySImplesApplication.java
@@ -0,0 +1,13 @@
+package com.PicPaySImplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySImplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySImplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PicPaySImplificado/PicPaySImplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PicPaySImplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPaySImplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
